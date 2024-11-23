67c64f76ec66f80a86f0b055ffd47afe771a20d7
mhuy2k11 <mhuy14032011@gmail.com>
2024-11-23 20:28:47 +0700

    Add files via upload

---
+++ js/math.js
@@ -0,0 +1,190 @@
+<!doctype html>
+<html dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no">
+        <meta name="robots" content="NOODP">
+        <noscript>
+            <meta http-equiv="refresh" content="0; url=/nojs/splash">
+        </noscript>
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-status-bar-style" content="black">
+        <meta name="apple-mobile-web-app-title" content="Duolingo">
+        <meta name="google" content="notranslate">
+        <meta name="mobile-web-app-capable" content="yes">
+        <meta name="apple-itunes-app" content="app-id=570060128">
+        <meta name="facebook-domain-verification" content="mwudgypvvgl4fekxjk5rpk3eqg7ykt">
+        <link rel="apple-touch-icon" href="https://d35aaqx5ub95lt.cloudfront.net/images/duolingo-touch-icon2.png">
+        <link rel="icon" href="https://d35aaqx5ub95lt.cloudfront.net/favicon.ico">
+        <script src="https://cdn.cookielaw.org/scripttemplates/otSDKStub.js" data-document-language="true" charset="UTF-8" data-domain-script="55a7f100-001e-4085-87ec-1a6d522f7d67"></script>
+        <script>
+            function OptanonWrapper() {}
+        </script>
+        <script async defer="defer" src="https://www.recaptcha.net/recaptcha/enterprise.js?render=6LcLOdsjAAAAAFfwGusLLnnn492SOGhsCh-uEAvI"></script>
+        <link rel="manifest" href="/manifest.json">
+        <meta name="twitter:site" content="@duolingo"/>
+        <meta name="twitter:image" content="https://www.duolingo.com/images/facebook/duo200.png"/>
+        <meta name="twitter:description" content="Học ngoại ngữ qua các trò chơi. Vừa miễn phí, rất vui và thực sự hiệu quả." data-react-helmet="true" data-rh="true"/>
+        <meta name="twitter:title" content="Duolingo - Học ngoại ngữ miễn phí @duolingo"/>
+        <meta name="twitter:card" content="summary"/>
+        <meta property="og:url" content="https://www.duolingo.com/alphabets/en/pronunciation"/>
+        <meta property="og:image:height" content="630"/>
+        <meta property="og:type" content="website"/>
+        <meta property="og:description" content="Học ngoại ngữ qua các trò chơi. Vừa miễn phí, rất vui và thực sự hiệu quả." data-react-helmet="true" data-rh="true"/>
+        <meta property="og:image:width" content="1200"/>
+        <meta property="og:title" content="Học ngoại ngữ miễn phí"/>
+        <meta property="fb:app_id" content="234536436609303"/>
+        <meta property="og:image" content="https://www.duolingo.com/images/facebook/duo-08523a2.jpg"/>
+        <meta property="og:site_name" content="Duolingo"/>
+        <title data-react-helmet="true" data-rh="true">Duolingo</title>
+        <meta name="keywords" content="học, tiếng Tây ban Nha, tiếng Đức, tiếng Pháp, tiếng Bồ Đào Nha, tiếng Ý, tiếng Anh, miễn phí, bài học, khóa học, ngôn ngữ, học, flashcard"/>
+        <meta name="description" content="Duolingo là cách phổ biến nhất trên thế giới để học ngôn ngữ. Miễn phí 100%, vui nhộn và dựa trên khoa học. Hãy thực hành trực tuyến trên duolingo.com hoặc trên ứng dụng!" data-react-helmet="true" data-rh="true"/>
+        <link rel="alternate" hreflang="ar" href="https://ar.duolingo.com/alphabets/en/pronunciation"/>
+        <link rel="alternate" hreflang="bn" href="https://bn.duolingo.com/alphabets/en/pronunciation"/>
+        <link rel="alternate" hreflang="cs" href="https://cs.duolingo.com/alphabets/en/pronunciation"/>
+        <link rel="alternate" hreflang="de" href="https://de.duolingo.com/alphabets/en/pronunciation"/>
+        <link rel="alternate" hreflang="el" href="https://el.duolingo.com/alphabets/en/pronunciation"/>
+        <link rel="alternate" hreflang="en" href="https://www.duolingo.com/alphabets/en/pronunciation"/>
+        <link rel="alternate" hreflang="es" href="https://es.duolingo.com/alphabets/en/pronunciation"/>
+        <link rel="alternate" hreflang="fr" href="https://fr.duolingo.com/alphabets/en/pronunciation"/>
+        <link rel="alternate" hreflang="hi" href="https://hi.duolingo.com/alphabets/en/pronunciation"/>
+        <link rel="alternate" hreflang="hu" href="https://hu.duolingo.com/alphabets/en/pronunciation"/>
+        <link rel="alternate" hreflang="id" href="https://id.duolingo.com/alphabets/en/pronunciation"/>
+        <link rel="alternate" hreflang="it" href="https://it.duolingo.com/alphabets/en/pronunciation"/>
+        <link rel="alternate" hreflang="ja" href="https://ja.duolingo.com/alphabets/en/pronunciation"/>
+        <link rel="alternate" hreflang="ko" href="https://ko.duolingo.com/alphabets/en/pronunciation"/>
+        <link rel="alternate" hreflang="nl" href="https://nl-nl.duolingo.com/alphabets/en/pronunciation"/>
+        <link rel="alternate" hreflang="pl" href="https://pl.duolingo.com/alphabets/en/pronunciation"/>
+        <link rel="alternate" hreflang="pt" href="https://pt.duolingo.com/alphabets/en/pronunciation"/>
+        <link rel="alternate" hreflang="ro" href="https://ro.duolingo.com/alphabets/en/pronunciation"/>
+        <link rel="alternate" hreflang="ru" href="https://ru.duolingo.com/alphabets/en/pronunciation"/>
+        <link rel="alternate" hreflang="sv" href="https://sv.duolingo.com/alphabets/en/pronunciation"/>
+        <link rel="alternate" hreflang="ta" href="https://ta.duolingo.com/alphabets/en/pronunciation"/>
+        <link rel="alternate" hreflang="te" href="https://te.duolingo.com/alphabets/en/pronunciation"/>
+        <link rel="alternate" hreflang="th" href="https://th.duolingo.com/alphabets/en/pronunciation"/>
+        <link rel="alternate" hreflang="tl" href="https://tl.duolingo.com/alphabets/en/pronunciation"/>
+        <link rel="alternate" hreflang="tr" href="https://tr.duolingo.com/alphabets/en/pronunciation"/>
+        <link rel="alternate" hreflang="uk" href="https://uk.duolingo.com/alphabets/en/pronunciation"/>
+        <link rel="alternate" hreflang="vi" href="https://vi.duolingo.com/alphabets/en/pronunciation"/>
+        <link rel="alternate" hreflang="x-default" href="https://www.duolingo.com/alphabets/en/pronunciation"/>
+        <link rel="alternate" hreflang="zh" href="http://www.duolingo.cn/alphabets/en/pronunciation"/>
+        <link rel="canonical" href="https://www.duolingo.com/alphabets/en/pronunciation" id="canonical"/>
+        <script>
+            window.duo = {
+                "removeNativeAppBanner": false,
+                "uiLanguage": "vi",
+                "oldWebUrlWhitelist": ["^/comment/", "^/design$", "^/discussion$", "^/topic/"]
+            }
+        </script>
+        <script>
+            window.duo.courseSplashRedesign = !0
+        </script>
+        <script>
+            window.duo.enableSentry = !0
+        </script>
+        <script>
+            window.duo.useImmer = "immer-freeze"
+        </script>
+        <script defer="defer" src="https://d35aaqx5ub95lt.cloudfront.net/js/manifest-72acd83e.js"></script>
+        <script defer="defer" src="https://d35aaqx5ub95lt.cloudfront.net/js/features-91ec4487.js"></script>
+        <script>
+            "use strict";
+            (self.webpackChunk = self.webpackChunk || []).push([[7979], {
+                6725: (e, o, f) => {
+                    var n = f(83141)
+                      , d = f(38020);
+                    const t = f.p + "vendor/68d22534874c554b0c9ebabea379fc60.woff2"
+                      , a = new FontFace("din-round",`url(${d}) format("woff2")`,{
+                        style: "normal",
+                        weight: "500"
+                    })
+                      , r = new FontFace("din-round",`url(${n}) format("woff2")`,{
+                        style: "normal",
+                        weight: "700"
+                    })
+                      , c = new FontFace("feather",`url(${t}) format("woff2")`,{
+                        style: "normal",
+                        weight: "700"
+                    });
+                    document.fonts.add(a),
+                    document.fonts.add(r),
+                    document.fonts.add(c),
+                    a.load(),
+                    r.load()
+                }
+                ,
+                83141: (e, o, f) => {
+                    e.exports = f.p + "vendor/dcf3e686284c5e7eeca4f8e200392c01.woff2"
+                }
+                ,
+                38020: (e, o, f) => {
+                    e.exports = f.p + "vendor/8df26391f330be315074ddd7b74b66b8.woff2"
+                }
+            }, e => {
+                e(e.s = 6725)
+            }
+            ])
+        </script>
+        <script defer="defer" src="https://d35aaqx5ub95lt.cloudfront.net/js/8553-a0eeda7a.js"></script>
+        <script defer="defer" src="https://d35aaqx5ub95lt.cloudfront.net/js/polyfills-d1e2cb95.js"></script>
+        <script defer="defer" src="https://d35aaqx5ub95lt.cloudfront.net/js/9036-da914fe6.js"></script>
+        <script defer="defer" src="https://d35aaqx5ub95lt.cloudfront.net/js/strings/vi-f0fe53c7.js"></script>
+        <script defer="defer" src="https://d35aaqx5ub95lt.cloudfront.net/js/1189-63944f2c.js"></script>
+        <script defer="defer" src="https://d35aaqx5ub95lt.cloudfront.net/js/app-032b8e92.js"></script>
+        <link href="https://d35aaqx5ub95lt.cloudfront.net/css/1189-bdca9d96.css" rel="stylesheet">
+        <link href="https://d35aaqx5ub95lt.cloudfront.net/css/app-2858b528.css" rel="stylesheet">
+    </head>
+    <body>
+        <noscript>
+            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TX6Z97C" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+        </noscript>
+        <div class="fs-unmask" id="root" onclick=""></div>
+        <div class="fs-unmask" id="overlays" onclick=""></div>
+        <script>
+            if ("/errors/not-supported.html" !== window.location.pathname)
+                for (var supportsAbortController = ("AbortController"in window && "Request"in window && Object.hasOwnProperty.call(Request.prototype, "signal")), supportsElementAnimate = ("animate"in Element.prototype), supportsES2015 = function() {
+                    if ("undefined" == typeof Symbol || "undefined" == typeof Proxy)
+                        return !1;
+                    try {
+                        return new Function("(a = 0) => a")(),
+                        new Function("class MyEvent extends Event{}")(),
+                        !1 === new Function("return new Boolean(Symbol.match)")() ? !1 : (new Function("new.target")(),
+                        new Function('class ಠ_ಠ extends Array {constructor(j = "a", ...c) {const q = (({u: e}) => {return { [`s${c}`]: Symbol(j) };})({});super(j, q, ...c);}}new Promise((f) => {const a = function* (){return "𠮷".match(/./u)[0].length === 2 || true;};for (let vre of a()) {const [uw, as, he, re] = [new Set(), new WeakSet(), new Map(), new WeakMap()];break;}f(new Proxy({}, {get: (han, h) => h in han ? han[h] : "42".repeat(0o10)}));}).then(bi => new ಠ_ಠ(bi.rd));')(),
+                        !!new Function("return (a, b,) => a.padStart(5, '0') === '0000x' && Object.values(b).length === 2")()("x", {
+                            a: 1,
+                            b: 2
+                        }))
+                    } catch (e) {
+                        return !1
+                    }
+                }(), supportsEs2019 = ("flat"in Array.prototype && "flatMap"in Array.prototype && "fromEntries"in Object && "trimStart"in String.prototype && "trimEnd"in String.prototype && "description"in Symbol.prototype), supportsIntersectionObserver = ("IntersectionObserver"in window && "IntersectionObserverEntry"in window && "intersectionRatio"in window.IntersectionObserverEntry.prototype && "isIntersecting"in window.IntersectionObserverEntry.prototype), supportsWebAssembly = ("WebAssembly"in window), supportsResizeObserver = ("ResizeObserver"in window), features = [supportsEs2019, supportsWebAssembly, supportsAbortController, supportsElementAnimate, supportsES2015, supportsIntersectionObserver, supportsResizeObserver], i = 0; i < features.length; i++)
+                    features[i] || (window.location.href = "/errors/not-supported.html");
+            window.duo.disableMonetization = /[?&]utm_source=pwa_launch/.test(window.location.search),
+            window.duo.l10n = {
+                strings: {},
+                contextualVariableMapping: {}
+            },
+            window.duo.uiLanguage = window.duo.uiLanguage || "en",
+            window.duo.version = "1.238.3",
+            window.duo.versionHash = "54d77ddf58e39255a104bb27aa6cbb14e280dcdf";
+            var isAbc = /^\/abc/.test(window.location.pathname)
+              , HIDE_APP_SMART_BANNER_PATH = ["/2022-campaigns", "/share/sm", "/share-direct/sm", "/super", "/plus", "/getplus", "/youtubeplus"];
+            if (HIDE_APP_SMART_BANNER_PATH.includes(window.location.pathname))
+                (el = document.querySelector("[name=apple-itunes-app]")) && el.remove();
+            else if (isAbc) {
+                var el;
+                (el = document.querySelector("[name=apple-itunes-app]")) && el.setAttribute("content", "app-id=1440502568")
+            }
+            try {
+                if (window.location.pathname.startsWith("/reset_password") && window.location.search.includes("email=")) {
+                    const e = new URLSearchParams(window.location.search);
+                    e.delete("email"),
+                    window.history.replaceState(null, "", `${window.location.pathname}?${e.toString()}`)
+                }
+            } catch (e) {
+                console.error("Error removing email query parameter on reset_password page", e)
+            }
+        </script>
+    </body>
+</html>
+
